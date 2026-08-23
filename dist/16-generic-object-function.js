5c0dfb03c1f204add076fc473021f36b9c7fa14f
Aminul islam <aminulislam424842@gmail.com>
2026-08-23 22:12:29 +0600

    Add generic function to access object properties

---
+++ dist/16-generic-object-function.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function getProperty(obj, key) {
+    return obj[key];
+}
+const user = {
+    name: "aminul",
+    age: 21
+};
+console.log(getProperty(user, "name"));
+console.log(getProperty(user, "age"));
+//# sourceMappingURL=16-generic-object-function.js.map
---
+++ src/16-generic-object-function.ts
@@ -0,0 +1,11 @@
+function getProperty<T, K extends keyof T>(obj: T, key: K): T[K] {
+  return obj[key]
+}
+
+const user = {
+  name: "aminul",
+  age: 21
+}
+
+console.log(getProperty(user, "name"))
+console.log(getProperty(user, "age"))
